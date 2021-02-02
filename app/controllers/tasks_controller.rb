98ddc280cbe7eb241f4092d43a4c7de5a13cebac
genki.hagiiwa <rockremover@gmail.com>
2021-02-02 09:24:47 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'Taskが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:denger] = 'Taskが投稿されませんでした'
+      render :new
+    end  
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+        
+    if @task.save
+      flash[:success] = 'Taskが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:denger] = 'Taskが投稿されませんでした'
+      render :new
+    end  
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'Taskは削除されました'
+    redirect_to tasks_url
+  end
+end
+
+private
+def task_params
+  params.require(:task).permit(:content)
+end  
