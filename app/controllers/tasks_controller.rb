a3e1693acfec210dc49f9289c912bf2188ddece1
Ubuntu <ubuntu@ip-172-31-27-109.ap-northeast-1.compute.internal>
2021-02-10 10:41:47 +0000

    delete unnecessary method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,9 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:show, :edit, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    if logged_in?
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    else
-      redirect_to '/login'
-    end
   end
 
   def show
@@ -56,9 +51,6 @@ class TasksController < ApplicationController
 
   private
   
-  def set_task
-    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-  end
 
   def task_params
    params.require(:task).permit(:content, :status, :references)
