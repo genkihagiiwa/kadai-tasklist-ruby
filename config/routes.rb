bc1b9d66836585a60ac0545b13412ed353267b22
Ubuntu <ubuntu@ip-172-31-27-109.ap-northeast-1.compute.internal>
2021-02-10 09:54:10 +0000

    delete toppages routeing

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get 'toppages/index'
-  
   get 'signup', to: 'users#new'
   
   resources :users, only: [:new, :create]
