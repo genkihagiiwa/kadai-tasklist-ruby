21250b81ecdf471030b01860ffeec39578c8946f
Ubuntu <ubuntu@ip-172-31-27-109.ap-northeast-1.compute.internal>
2021-02-10 06:33:16 +0000

    add foregin key, login system and checkin system

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,17 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    else
+      redirect_to '/login'
+    end
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,10 +20,11 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
+    @task.user_id = current_user.id 
     
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
-      redirect_to @task
+      redirect_to '/'
     else
       flash.now[:denger] = 'Taskが投稿されませんでした'
       render :new
@@ -24,10 +32,11 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
-        
+    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = 'Taskが正常に更新されました'
       redirect_to @task
@@ -48,11 +57,17 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def task_params
-   params.require(:task).permit(:content, :status)
+   params.require(:task).permit(:content, :status, :references)
   end  
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+    redirect_to '/'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+  
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  root to: 'tasks#index'
+
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get 'toppages/index'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-
   
   get 'signup', to: 'users#new'
   
